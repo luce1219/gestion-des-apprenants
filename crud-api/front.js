98846083c2696f5e62e669bf616643602975dcd9
luce1219 <madjinoumounguenguifirmine@gmail.com>
2024-02-21 12:17:18 +0100

    affichage sur le navigateur

--- crud-api/front.js
+++ crud-api/front.js
@@ -1,17 +1,16 @@
-fetch('http://localhost:3000/apprenant')
-  .then((response) => response.json())
-  .then((data) => {
-    const div = document.getElementById('app');
+// fetch('http://localhost:3000/apprenants')
+// .then((response) => response.json())
+// .then((data) => {
+//     const div = document.getElementById('app');
 
-    data.forEach((apprenant) => {
-      console.log(apprenant);
-      div.innerHTML += `
-        <h2> Nom : ${apprenant.nom}</h2>
-        <h2> Prénom: ${apprenant.prenom}</h2>
-        <h2> Numéro: ${apprenant.numéro_de_téléphone}</h2>
-      `;
-    });
-  })
-  .catch((error) => {
-    console.error('Erreur lors de la requête fetch :', error);
-  });
+//     data.forEach((apprenant) => {
+//         div.innerHTML += `
+//             <h2> Nom : ${apprenant.nom}</h2>
+//             <h2> Prénom: ${apprenant.prenom}</h2>
+//             <h2> Numéro: ${apprenant.numéro_de_téléphone}</h2>
+//         `;
+//     });
+// })
+// .catch((error) => {
+//     console.error('Erreur lors de la requête fetch :', error);
+// });
